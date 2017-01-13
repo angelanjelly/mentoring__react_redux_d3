f01e43d05998e533fd3f589c7623fcd7ff8f9637
Angela M. Choi <angela.minchoi@gmail.com>
2017-01-12 20:27:23 -0800

    added map & valueSortFn

--- client/src/exercise-1-react-only/containers/StoreRsvpWordFrequencyTable.js
+++ client/src/exercise-1-react-only/containers/StoreRsvpWordFrequencyTable.js
@@ -5,8 +5,21 @@ const mapStateToProps = (state) => {
   return {
     keyHead: 'Word',
     valueHead: 'Count',
-    // map: ?,
-    // valueSortFn: ?,
+    map: state.get('rsvps').get('topics'),
+    valueSortFn: (a, b) => {
+      if (a[1] > b[1])
+        return -1;
+      else if (a[1] === b[1]) {
+        if (a[0] < b[0])
+          return -1;
+        else if (a[0] === b[0])
+          return 0;
+        else
+          return 1;
+      }
+      else
+        return 1;
+    },
     limit: 10
   };
 };
